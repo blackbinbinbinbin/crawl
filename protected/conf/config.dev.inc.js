55c1bc3b1508ce9fb088828c17648e638b67c0d7
blackbinbin <blackbinbin@foxmail.com>
2018-08-27 10:51:44 +0800

    fix config

--- protected/conf/config.dev.inc.js
+++ protected/conf/config.dev.inc.js
@@ -31,53 +31,15 @@ dbInfo['crawl'] = {
     port : 3306,
     connectionLimit : 100
 };
-//
-// dbInfo['dw_pc'] = {
-//     host : '61.160.36.225',
-//     user : 'ojiatest',
-//     password : 'ojia305',
-//     database : 'dw_pc',
-//     port : 3306,
-//     connectionLimit : 100
-// };
-//
-// dbInfo['dw_pc_data'] = {
-//     host : '61.160.36.225',
-//     user : 'ojiatest',
-//     password : 'ojia305',
-//     database : 'dw_pc_data',
-//     port : 3306,
-//     connectionLimit : 100
-// };
-//
+
 let redisInfo = {};
-// redisInfo['name_serv'] = {
-//     'host' : '61.160.36.225',
-//     'port' : 6405,
-//     'pwd' : 'ojia123',
-//     'db' : 1,
-//     'connet_timeout' : 0
-// };
-//
-// redisInfo['Web'] = {
-//     host : '61.160.36.225',
-//     port : 6407,
-//     pwd : 'ojia123',
-//     db : 1
-// };
-//
+
 redisInfo['logstash_redis'] = {
     host : '61.160.36.225',
     port : 6407,
     pwd : 'ojia123',
     db : 2
 };
-//
-// redisInfo['logic'] = {
-//     host : "61.160.36.225",
-//     port : 6404,
-//     pwd : "ojia123"
-// };
 
 process.env.PORT = 9998;
 //
--- protected/conf/config.form.inc.js
+++ protected/conf/config.form.inc.js
@@ -5,55 +5,19 @@ global['TYPE_MODULE_CALL'] = 'spider_self_call';
 
 // let dbInfo = {};
 //
-// dbInfo['Web'] = {
-//     host : '10.25.68.139',
-//     user : 'ojuser',
-//     password : 'qs45MWKf',
-//     database : 'Web',
-//     port : 6308,
-//     connectionLimit : 100
-// };
-//
-// dbInfo['Report'] = {
-//     host : '10.25.68.139',
-//     user : 'ojuser',
-//     password : 'qs45MWKf',
-//     database : 'Report',
-//     port : 6308,
-//     connectionLimit : 100
-// };
-//
-// dbInfo['crawl'] = {
-//     host : '10.25.68.139',
-//     user : 'ojuser',
-//     password : 'qs45MWKf',
-//     database : 'crawl',
-//     port : 6308,
-//     connectionLimit : 100
-// };
-//
-// dbInfo['dw_pc'] = {
-//     host : '10.20.167.232',
-//     user : 'dw_sy_rw',
-//     password : 'Yn7wx9g5QG',
-//     database : 'dw_pc',
-//     port : 6301,
-//     connectionLimit : 100
-// };
-//
-// dbInfo['dw_pc_data'] = {
-//     host : '10.20.167.232',
-//     user : 'dw_sy_rw',
-//     password : 'Yn7wx9g5QG',
-//     database : 'dw_pc_data',
-//     port : 6301,
+// dbInfo['数据库配置名称'] = {
+//     host : '服务器ip',
+//     user : '数据库用户名',
+//     password : '数据库用户密码',
+//     database : '库名',
+//     port : '端口',
 //     connectionLimit : 100
 // };
 
+
 // let redisInfo = {};
 //
-// redisInfo["logstash_redis"] = {"connet_timeout":"1","host":"10.20.164.64","port":"6399","pwd":"Fh7fVj8fSsKf"};
-// redisInfo["logic"] = {"host":"10.20.164.64","port":"6398","pwd":"Fh7fVj8fSsKf"};
+// redisInfo["redis配置名称"] = {"connet_timeout":"超时时间","host":"redis ip","port":"端口号","pwd":"密码"};
 
 process.env.PORT = 10000;
 //
